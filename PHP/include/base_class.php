cf9a7185016cc0e2e7002ce64e35c1b257c75fc9
Chen-Lung-Chin <lungchin.tw@gmail.com>
2019-03-30 10:01:35 +0700

    Improved: printSymbol

--- PHP/include/base_class.php
+++ PHP/include/base_class.php
@@ -5,6 +5,17 @@ require_once 'interface.php';
 
 
 class BaseClass implements IInterface {
+    private const CONST_BASE_CLASS_DESC = 'class BaseClass implements IInterface';
+
+    static function printSymbol() {
+        echo 'Desc: '.self::CONST_BASE_CLASS_DESC.PHP_EOL;
+        echo 'self::class: '.self::class.PHP_EOL;
+        echo 'static::class: '.static::class.PHP_EOL;
+        echo '__CLASS__: '.__CLASS__.PHP_EOL;
+        echo '__METHOD__: '.__METHOD__.PHP_EOL;
+        echo '__FUNCTION__: '.__FUNCTION__.PHP_EOL;
+    }
+
     function __construct() {
         echo self::class.': '.__METHOD__.PHP_EOL;
     }
