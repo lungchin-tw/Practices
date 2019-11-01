e81185ee527919d63921eb3831bbb502eef6b189
Chen-Lung-Chin <lungchin.tw@gmail.com>
2019-11-01 16:01:21 +0800

    Done: Generic View

--- Django/generic_view/polls/urls.py
+++ Django/generic_view/polls/urls.py
@@ -7,10 +7,7 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
     path( '', views.IndexView.as_view(), name='index' ),
-    # path( 'index/v2/', views.index_v2, name='index_v2' ),
-    # path( 'index/v3/', views.index_v3, name='index_v3' ),
     path( '<int:pk>/', views.DetailView.as_view(), name='detail' ),
-    # path( '<int:question_id>/detail/v2/', views.detail_v2, name='detail_v2' ),
     path( '<int:pk>/result/', views.ResultView.as_view(), name='result' ),
     path( '<int:question_id>/vote/', views.vote, name='vote' ),
 ]
--- Django/generic_view/polls/views.py
+++ Django/generic_view/polls/views.py
@@ -68,68 +68,6 @@ def print_frame_info( fi ):
     )
 
 
-def index(request):
-    print_frame_info( getframeinfo(currentframe()) )
-    print_request(request)
-
-    latest_question_list = Question.objects.order_by( '-pub_date' )[:1]
-    context = '<br/>'.join( (q.question_text for q in latest_question_list) )
-
-    print( context )
-    return HttpResponse( context )
-
-
-def index_v2(request):
-    print_frame_info( getframeinfo(currentframe()) )
-
-    latest_question_list = Question.objects.order_by( '-pub_date' )[:2]
-    template = loader.get_template( 'polls/index.html' )
-    context = { 'latest_question_list': latest_question_list }
-
-    print( latest_question_list )
-    print( template )
-    print( context )
-
-    return HttpResponse( template.render( context, request ) )
-
-
-def index_v3(request):
-    print_frame_info( getframeinfo(currentframe()) )
-
-    latest_question_list = Question.objects.order_by( '-pub_date' )[:3]
-    context = { 'latest_question_list': latest_question_list }
-    return render( request, 'polls/index.html', context )
-
-
-def detail(request, question_id):
-    print_frame_info( getframeinfo(currentframe()) )
-    print_request(request)
-
-    try:
-        q = Question.objects.get( pk=question_id )
-    except Question.DoesNotExist:
-        raise Http404( 'Question %s does not exist.' % question_id )
-
-    return render( request, 'polls/detail.html', { 'question': q } )
-
-
-def detail_v2(request, question_id):
-    print_frame_info( getframeinfo(currentframe()) )
-    print_request(request)
-
-    print( 'question_id: %d' % question_id )
-    q = get_object_or_404( Question, pk=question_id )
-    return render( request, 'polls/detail_v2.html', { 'question': q } )
-
-
-def result(request, question_id):
-    print_frame_info( getframeinfo(currentframe()) )
-    print_request(request)
-
-    print( 'question_id: %d' % question_id )
-    return HttpResponse( 'You\'re looking at the result of question %s.' % question_id )
-
-
 def vote(request, question_id):
     print_frame_info( getframeinfo(currentframe()) )
     print_request(request)
@@ -146,7 +84,7 @@ def vote(request, question_id):
             'error_message': "Invalid Choice!!!"
         }
 
-        return render(request, 'polls/detail_v2.html', context )
+        return render(request, 'polls/detail.html', context )
     else:
         selected_choice.votes += 1
         selected_choice.save()
