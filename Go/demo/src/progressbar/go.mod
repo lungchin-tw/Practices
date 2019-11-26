4dc481b211dea6f77cda821870460d3c27d0797c
longchin <longchin@msa>
2019-11-26 19:09:02 +0700

    CHG: progress demo

---
+++ Go/demo/src/progressbar/go.mod
@@ -0,0 +1,5 @@
+module main
+
+require github.com/schollz/progressbar/v2 v2.14.2
+
+go 1.13
--- Go/demo/src/progressbar/progressbar_demo.go
+++ Go/demo/src/progressbar/progressbar_demo.go
@@ -3,13 +3,24 @@ package main
 import (
 	"time"
 
-	"github.com/schollz/progressbar"
+	progressbar "github.com/schollz/progressbar/v2"
 )
 
 func main() {
-	bar := progressbar.New(100)
-	for index := 0; index < 100; index++ {
-		time.Sleep(100 * time.Millisecond)
-		bar.Add(1)
+	{
+		bar := progressbar.New(1000)
+		for index := 0; index < 10; index++ {
+			time.Sleep(100 * time.Millisecond)
+			bar.Add(1)
+		}
 	}
+
+	{
+		bar := progressbar.NewOptions(100, progressbar.OptionSetRenderBlankState(false))
+		for index := 0; index < 100; index++ {
+			time.Sleep(100 * time.Millisecond)
+			bar.Add(1)
+		}
+	}
+
 }
