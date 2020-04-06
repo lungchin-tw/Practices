51af67c1f6f2f55a18b60aeab5bb935f817a85e7
Chen-Lung-Chin <jacky@Jackys-MBP-13.local>
2020-04-06 18:22:43 +0800

    NEW: WebGL Exercise

--- Go/demo/src/env/print_env.go
+++ Go/demo/src/env/print_env.go
@@ -10,6 +10,7 @@ func main() {
 	fmt.Println("runtime.GOARCH: ", runtime.GOARCH)
 	fmt.Println("runtime.GOROOT: ", runtime.GOROOT())
 	fmt.Println("runtime.NumCPU: ", runtime.NumCPU())
+	fmt.Println("runtime.GOMAXPROCS(0): ", runtime.GOMAXPROCS(0))
 	fmt.Println("runtime.NumCgoCall: ", runtime.NumCgoCall())
 	fmt.Println("runtime.NumGoroutine: ", runtime.NumGoroutine())
 	fmt.Println("runtime.Version: ", runtime.Version())
---
+++ Web/Canvas/canvas-demo.js
@@ -0,0 +1,7 @@
+var canvas = document.getElementById("canvas-demo");
+
+var context = canvas.getContext('2d');
+context.font = '20pt Calibri';
+context.fillStyle = 'green';
+context.fillText('Welcome to My Canvas Demo', 70, 70)
+
