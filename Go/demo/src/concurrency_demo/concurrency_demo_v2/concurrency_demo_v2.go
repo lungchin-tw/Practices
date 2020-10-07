82ef116099c3943b8759cda29e3a537ef3da5b06
Chen-Lung-Chin <jacky@jackydeMacBook-Pro.local>
2020-10-07 15:38:42 +0800

    NEW: concurrency demo v2

---
+++ Go/demo/src/concurrency_demo/concurrency_demo_v2/concurrency_demo_v2.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+type TPlayer struct {
+	ID uint64
+}
+
+var ShutdownChan chan bool = make(chan bool)
+var PlayerChan chan *TPlayer = make(chan *TPlayer, 1000)
+var NumPlayer int
+var TotalNumPlayer int
+var GlobalPlayerUniqueID uint64
+
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	go func() {
+		for {
+			select {
+			case player := <-PlayerChan:
+				NumPlayer++
+				fmt.Printf("[%v/%v], %v\n", NumPlayer, TotalNumPlayer, *player)
+			default:
+				fmt.Println("Select Default: Sleep:", 1)
+				time.Sleep(1 * time.Second)
+			}
+		}
+	}()
+
+	go func() {
+		for {
+			num := rand.Intn(50) + rand.Intn(50)
+			TotalNumPlayer += num
+			fmt.Println("Total Generated Num Player:", TotalNumPlayer)
+
+			for i := 0; i < num; i++ {
+				GlobalPlayerUniqueID++
+				PlayerChan <- &TPlayer{ID: GlobalPlayerUniqueID}
+			}
+
+			sleep := time.Duration(rand.Intn(10)) * time.Second
+			fmt.Println("Sleep:", sleep)
+			time.Sleep(sleep)
+		}
+	}()
+
+	<-ShutdownChan
+}
