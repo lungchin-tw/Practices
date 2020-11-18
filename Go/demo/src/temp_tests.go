df3f7804bf41444f6850979af719ff28a8325bd4
Chen-Lung-Chin <jacky@jackydeMacBook-Pro.local>
2020-11-18 14:49:41 +0800

    NEW: termui demos

--- Go/demo/src/temp_tests.go
+++ Go/demo/src/temp_tests.go
@@ -2,13 +2,23 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"time"
+)
+
+const (
+	EGET_NONE = -1 + iota
+	EGET_INVALID_STAGE
+	EGET_INVALID_ACTION
+	EGET_INVALID_TYPE
+	EGET_ACTION_ALREADY
+	EGET_PLAYER_EXISTS
+	EGET_SCENE_IS_FULL
 )
 
 func main() {
-	rand.Seed(time.Now().UnixNano())
-	fmt.Println(rand.Intn(3))
-	fmt.Println(rand.Intn(3))
-	fmt.Println(rand.Intn(3))
+	fmt.Println(EGET_NONE)
+	fmt.Println(EGET_INVALID_STAGE)
+	fmt.Println(EGET_INVALID_ACTION)
+	fmt.Println(EGET_INVALID_TYPE)
+	fmt.Println(EGET_ACTION_ALREADY)
+
 }
