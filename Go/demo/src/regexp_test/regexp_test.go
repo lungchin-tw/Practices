4cf1cb7031897cbb732121824d20b3028f41bad8
Chen-Lung-Chin <jacky@jackydeMacBook-Pro.local>
2021-07-02 18:39:41 +0800

    NEW: regexp tests

--- Go/demo/src/regexp_test/regexp_test.go
+++ Go/demo/src/regexp_test/regexp_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"regexp"
+	"strings"
 	"testing"
 )
 
@@ -30,7 +31,11 @@ func TestRegexpCase01(t *testing.T) {
 
 func TestRegexpCase02(t *testing.T) {
 	p := "I am learning Go language"
-	re, err := regexp.Compile("[a-z]{2,4}")
+	expr := "[a-z]{2,4}"
+	t.Log("TEXT:", p)
+	t.Log("EXPR:", expr)
+
+	re, err := regexp.Compile(expr)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -43,26 +48,95 @@ func TestRegexpCase02(t *testing.T) {
 
 func TestRegexpCase03(t *testing.T) {
 	p := "I am learning Go language"
-	re, err := regexp.Compile("am(.*)lang(.*)")
+	expr := "am(.*)lang(.*)"
+	t.Log("TEXT:", p)
+	t.Log("EXPR:", expr)
+
+	re, err := regexp.Compile(expr)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	re.FindStringSubmatch(p)
 	t.Logf("re.FindStringSubmatch(): %#v", re.FindStringSubmatch(p))
 	t.Logf("re.FindAllStringSubmatch: %#v", re.FindAllStringSubmatch(p, -1))
 }
 
 func TestRegexpCase04(t *testing.T) {
-	src := []byte(`
+	p := []byte(`
 		call hello alice
 		hello bob
 		call hello eve
 	`)
-	pat := regexp.MustCompile(`(?m)(call)\s+(?P<cmd>\w+)\s+(?P<arg>.+)\s*$`)
+	expr := `(?m)(call)\s+(?P<cmd>\w+)\s+(?P<arg>.+)\s*$`
+	t.Log("TEXT:", string(p))
+	t.Log("EXPR:", expr)
+
+	pat := regexp.MustCompile(expr)
 	res := []byte{}
-	for _, s := range pat.FindAllSubmatchIndex(src, -1) {
-		res = pat.Expand(res, []byte("$cmd('$arg')\n"), src, s)
+	for _, s := range pat.FindAllSubmatchIndex(p, -1) {
+		res = pat.Expand(res, []byte("$cmd('$arg')\n"), p, s)
 	}
 	fmt.Println(string(res))
+}
+
+func TestRegexpCase05(t *testing.T) {
+	p := "I am learning Go language"
+	expr := "a[a-z]"
+	t.Log("TEXT:", p)
+	t.Log("EXPR:", expr)
+
+	re, err := regexp.Compile(expr)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log("re.FindAllString():", re.FindAllString(p, -1))
+}
+
+func TestRegexpCase06(t *testing.T) {
+	texts := map[string]string{
+		"/:param":             "/test",
+		"/users/:uid([0-9]+)": "/users/1234",
+	}
+
+	for k, v := range texts {
+		t.Run(v, func(t *testing.T) { RunTestRegexpCase06(k, v, t) })
+	}
+}
+
+func RunTestRegexpCase06(pattern string, value string, t *testing.T) {
+	t.Log("PATTERN BEFORE:", pattern)
+	t.Log("VALUE:", value)
+
+	parts := strings.Split(pattern, "/")
+	t.Logf("PARTS BEFORE: %#v", parts)
+
+	j := 0
+	params := make(map[int]string)
+	for i, v := range parts {
+		if strings.HasPrefix(v, ":") {
+			expr := "([^/]+)"
+			if index := strings.Index(v, "("); index != -1 {
+				expr = v[index:]
+				v = v[:index]
+			}
+
+			params[j] = v
+			parts[i] = expr
+			j++
+		}
+	}
+
+	t.Logf("PARAMS: %#v", params)
+	t.Logf("PARTS AFTER: %#v", parts)
+
+	pattern = strings.Join(parts, "/")
+	t.Log("PATTERN AFTER:", pattern)
+
+	if regex, err := regexp.Compile(pattern); err != nil {
+		t.Error(err)
+	} else {
+		t.Log("regex.MatchString:", regex.MatchString(value))
+		t.Log("regex.FindStringSubmatch:", regex.FindStringSubmatch(value))
+	}
 }
