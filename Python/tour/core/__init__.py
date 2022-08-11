31311d9146f4587f74593def7683914e4125ad5e
lungchin <lungchin.tw@gmail.com>
2022-08-11 17:15:52 +0800

    NEW: Python Decorator Tests.

--- Python/tour/core/__init__.py
+++ Python/tour/core/__init__.py
@@ -1,9 +1,14 @@
 
-import logging, sys
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
+import logging, sys, random
 
-logger.info(f'Loading {__file__}, __name__:{__name__}')
+
+random.seed()
+
+
+def getLogger(name: str):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+    return logger
 
 
 def func_desc(cls=None) -> str:
@@ -12,3 +17,7 @@ def func_desc(cls=None) -> str:
         return f'{frame.f_code.co_name}, {frame.f_code.co_filename}:{frame.f_lineno}'
     else:
         return f'{cls}:{frame.f_code.co_name}, {frame.f_code.co_filename}:{frame.f_lineno}'
+
+
+logger = getLogger(__name__)
+logger.info(f'Loading {__file__}, __name__:{__name__}')
