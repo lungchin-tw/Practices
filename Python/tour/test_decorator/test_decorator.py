afefcb1c950c48ca75a8bcab92dd120e546041eb
lungchin <lungchin.tw@gmail.com>
2022-08-11 17:15:52 +0800

    NEW: Python Decorator Tests.

--- Python/tour/test_decorator/test_decorator.py
+++ Python/tour/test_decorator/test_decorator.py
@@ -1,11 +1,64 @@
-import logging
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
 
+
+from core import *
+from  uuid import  uuid4
+import  random, datetime
+
+logger = getLogger(__name__)
 logger.info(f'Loading {__file__}, __name__:{__name__}')
 
-from core import func_desc
+def wraplog(func):
+    def wrapper(*args, **kwargs):
+        start = datetime.datetime.now()
+        logger.info(f'Started:{func}, args:{args}, kwargs:{kwargs}')
+        val = func(*args, **kwargs)
+        elapsed = datetime.datetime.now() - start
+        logger.info(f'Ended:{func}, Elapsed:{elapsed}')
+        return val
+    return wrapper
 
 
+@wraplog
+def hello(uuid, field='UUID'):
+    logger.info(
+        {
+            "Desc": func_desc(),
+            field: uuid,
+        }
+    )
+
+@wraplog
+def add(x, y: int) -> int:
+    logger.info(
+        {
+            "Desc": func_desc(),
+            "x": x,
+            "y": y,
+        }
+    )
+    return x + y
+
+class DemoClass:
+    @wraplog
+    def demo_decorated_method(self, msg:str):
+        logger.info(
+            {
+                "Desc": func_desc(),
+                "Message": msg,
+            }
+        )
+
+
+@wraplog
 def test_decorator():
+    range = 100
     logger.info({"Desc": func_desc()})
+
+    hello(uuid4())
+
+    logger.info({
+        "Result": add(random.randrange(range), random.randrange(range)),
+    })
+
+    DemoClass().demo_decorated_method('Hi, DemoClass')
+
